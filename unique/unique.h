d2a3e6aad12dda727192d2ce5eafdbb77f106ab8
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 13:20:43 +0300

    Add unique task

--- unique/unique.h
+++ unique/unique.h
@@ -4,5 +4,15 @@
 #include <stdexcept>
 
 std::vector<int> Unique(const std::vector<int>& data) {
-    throw std::runtime_error{"Not implemented"};
+    if (data.empty()) {
+        return std::vector<int>();
+    }
+    std::vector<int> res;
+    res.push_back(data[0]);
+    for (size_t i = 1; i < data.size(); ++i) {
+        if (data[i - 1] != data[i]) {
+            res.push_back(data[i]);
+        }
+    }
+    return res;
 }
