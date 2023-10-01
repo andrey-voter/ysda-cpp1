473c7597539d533d3f8e892e5edfed04a65a911f
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 18:58:10 +0300

    Add permutations task

--- permutations/permutations.h
+++ permutations/permutations.h
@@ -3,7 +3,6 @@
 #include <vector>
 #include <stdexcept>
 #include <numeric>
-#include <algorithm>
 
 void Generate(std::vector<int>& arr, size_t index, std::vector<std::vector<int>>& permutations) {
     if (index == arr.size()) {
@@ -17,6 +16,29 @@ void Generate(std::vector<int>& arr, size_t index, std::vector<std::vector<int>>
     }
 }
 
+bool CompareVectors(const std::vector<int>& a, const std::vector<int>& b) {
+    int size = std::min(a.size(), b.size());
+    for (int i = 0; i < size; ++i) {
+        if (a[i] < b[i]) {
+            return true;
+        }
+        if (a[i] > b[i]) {
+            return false;
+        }
+    }
+    return a.size() < b.size();
+}
+
+void CustomSort(std::vector<std::vector<int>>& vec) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        for (size_t j = i + 1; j < vec.size(); ++j) {
+            if (CompareVectors(vec[j], vec[i])) {
+                std::swap(vec[i], vec[j]);
+            }
+        }
+    }
+}
+
 std::vector<std::vector<int>> GeneratePermutations(size_t len) {
     std::vector<std::vector<int>> permutations;
     std::vector<int> initial;
@@ -24,6 +46,6 @@ std::vector<std::vector<int>> GeneratePermutations(size_t len) {
         initial.push_back(static_cast<int>(i));
     }
     Generate(initial, 0, permutations);
-    std::sort(permutations.begin(), permutations.end());
+    CustomSort(permutations);
     return permutations;
 }
