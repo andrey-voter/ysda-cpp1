5da069f28b7a4cbdee6821f695693787a6366617
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 16:52:54 +0300

    Add polish notation task

--- polish-notation/polish_notation.h
+++ polish-notation/polish_notation.h
@@ -2,7 +2,62 @@
 
 #include <string>
 #include <stdexcept>
+#include <vector>
+#include <stack>
+#include <set>
+
+std::vector<std::string> Split(const std::string& string, const std::string& delimiter = " ") {
+    if (string.empty()) {
+        return std::vector<std::string>();
+    }
+    std::vector<std::string> res;
+    auto delimiter_pos = string.find(delimiter);
+    size_t prev_pos = 0;
+    while (std::string::npos != delimiter_pos) {
+        if (string.substr(prev_pos, delimiter.size()) == delimiter) {
+            res.push_back("");
+            prev_pos = delimiter_pos + delimiter.size();
+            delimiter_pos = string.find(delimiter, delimiter_pos + delimiter.size());
+            continue;
+        }
+        res.push_back(string.substr(prev_pos, delimiter_pos - prev_pos));
+        prev_pos = delimiter_pos + delimiter.size();
+        delimiter_pos = string.find(delimiter, delimiter_pos + delimiter.size());
+    }
+    if (prev_pos < string.size()) {
+        res.push_back(string.substr(prev_pos));
+    } else {
+        if (prev_pos == string.size()) {
+            res.push_back("");
+        }
+    }
+    return res;
+}
 
 int EvaluateExpression(const std::string& expression) {
-    throw std::runtime_error{"Not implemented"};
+    auto tokens = Split(expression);
+    std::stack<std::string> stack;
+    std::set<std::string> ops{"+", "-", "*"};
+    for (std::string str : tokens) {
+        if (ops.count(str)) {
+            int op1 = std::stoi(stack.top());
+            stack.pop();
+            int op2 = std::stoi(stack.top());
+            stack.pop();
+            std::string res;
+            if (str == "+") {
+                res = std::to_string(op2 + op1);
+            } else {
+                if (str == "*") {
+                    res = std::to_string(op2 * op1);
+                } else {
+                    res = std::to_string(op2 - op1);
+                }
+            }
+            stack.push(res);
+        } else {
+            stack.push(str);
+        }
+    }
+    return std::stoi(stack.top());
 }
