1b1df2f0ceddb384481e257106d12451395fb89a
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 13:11:48 +0300

    Add binpow task

--- bin-pow/binpow.h
+++ bin-pow/binpow.h
@@ -3,5 +3,14 @@
 #include <stdexcept>
 
 int BinPow(int a, int64_t b, int c) {
-    throw std::runtime_error{"Not implemented"};
+    int result = 1;
+    a %= c;
+    while (b > 0) {
+        if (b % 2) {
+            result = (1LL * result * a) % c;
+        }
+        a = (1LL * a * a) % c;
+        b /= 2;
+    }
+    return (result + c) % c;
 }
