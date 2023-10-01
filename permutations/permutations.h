0aaf36e24b6cc5a17e9a451f07523b77399e307e
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 18:50:17 +0300

    Add permutations task

--- permutations/permutations.h
+++ permutations/permutations.h
@@ -2,7 +2,28 @@
 
 #include <vector>
 #include <stdexcept>
+#include <numeric>
+#include <algorithm>
+
+void Generate(std::vector<int>& arr, size_t index, std::vector<std::vector<int>>& permutations) {
+    if (index == arr.size()) {
+        permutations.push_back(arr);
+        return;
+    }
+    for (size_t i = index; i < arr.size(); ++i) {
+        std::swap(arr[index], arr[i]);
+        Generate(arr, index + 1, permutations);
+        std::swap(arr[index], arr[i]);
+    }
+}
 
 std::vector<std::vector<int>> GeneratePermutations(size_t len) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<std::vector<int>> permutations;
+    std::vector<int> initial;
+    for (size_t i = 0; i < len; ++i) {
+        initial.push_back(static_cast<int>(i));
+    }
+    Generate(initial, 0, permutations);
+    std::sort(permutations.begin(), permutations.end());
+    return permutations;
 }
