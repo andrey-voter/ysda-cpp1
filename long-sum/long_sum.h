8167722c62967f1371844717c3dbbc9d77951a90
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 20:45:21 +0300

    Add long_sum task

--- long-sum/long_sum.h
+++ long-sum/long_sum.h
@@ -2,7 +2,43 @@
 
 #include <string>
 #include <stdexcept>
+#include <algorithm>
+#include <iostream>
 
 std::string LongSum(const std::string& a, const std::string& b) {
-    throw std::runtime_error{"Not implemented"};
+    std::string res;
+    size_t a_size = a.size();
+    size_t b_size = b.size();
+    std::string a_copy(a);
+    std::string b_copy(b);
+
+    if (a_size < b_size) {
+        std::string zeros = std::string(b_size - a_size, '0');
+        std::reverse(a_copy.begin(), a_copy.end());
+        a_copy += zeros;
+        std::reverse(a_copy.begin(), a_copy.end());
+    } else if (a_size > b_size) {
+        std::string zeros = std::string(a_size - b_size, '0');
+        std::reverse(b_copy.begin(), b_copy.end());
+        b_copy += zeros;
+        std::reverse(b_copy.begin(), b_copy.end());
+    }
+
+    int in_mind = 0;
+    for (int i = a_copy.size() - 1; i >= 0; --i) {
+        int val_1 = a_copy[i] - '0';
+        int val_2 = b_copy[i] - '0';
+        int s = val_1 + val_2 + in_mind;
+        in_mind = 0;
+        if (s >= 10) {
+            in_mind = 1;
+            s %= 10;
+        }
+        res.push_back(s + '0');
+        if (!i && in_mind) {
+            res.push_back(in_mind + '0');
+        }
+    }
+    std::reverse(res.begin(), res.end());
+    return res;
 }
--- long-sum/test.cpp
+++ long-sum/test.cpp
@@ -2,6 +2,19 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Your test") {
+TEST_CASE("Simple test") {
     CHECK(LongSum("2", "2") == "4");
+    CHECK(LongSum("0", "1") == "1");
+    CHECK(LongSum("0", "0") == "0");
+    CHECK(LongSum("111", "111") == "222");
+    CHECK(LongSum("123", "456") == "579");
+    CHECK(LongSum("999", "999") == "1998");
+
+}
+
+TEST_CASE("Different sizes test") {
+    CHECK(LongSum("237451699741", "2227893") == "237453927634");
+    CHECK(LongSum("7135469354213", "78876397517954397") == "78883532987308610");
+    CHECK(LongSum("15863546790182703", "752143002251372444") == "768006549041555147");
+
 }
