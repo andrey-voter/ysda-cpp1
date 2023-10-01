d14ed2cb12c9b324210716c39c8cf3bd599873af
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 21:21:22 +0300

    Add diff-pairs task

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -3,7 +3,19 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
+#include <algorithm>
+#include <map>
+#include <set>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
-    throw std::runtime_error{"Not implemented"};
+    std::map<int, int> freq;
+    int ans = 0;
+    for (size_t i = 0; i < data.size(); ++i) {
+        int complement = x - data[i];
+        if (freq.contains(complement)) {
+            ans += freq[complement];
+        }
+        freq[data[i]]++;
+    }
+    return ans;
 }
--- diff-pairs/test.cpp
+++ diff-pairs/test.cpp
@@ -2,5 +2,8 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Your tests here") {
+TEST_CASE("Simple test") {
+    CHECK(CountPairs({1, 2, 2, 3, 4}, 4) == 2);
+    CHECK(CountPairs({1, 2, 2, 3, 4, 2}, 4) == 4);
+    CHECK(CountPairs({}, 0) == 0);
 }
