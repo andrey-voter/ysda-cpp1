3789179505e5a151ace468f267c6d4447e87e379
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 17:14:54 +0300

    Add word-count task

--- word-count/word_count.h
+++ word-count/word_count.h
@@ -2,7 +2,32 @@
 
 #include <string>
 #include <stdexcept>
+#include <unordered_set>
+
+char my_tolower(char ch) {
+    return static_cast<char>(std::tolower(static_cast<unsigned char>(ch)));
+}
+
+bool my_isalpha(char ch) {
+    return std::isalpha(static_cast<unsigned char>(ch));
+}
 
 int DifferentWordsCount(const std::string& string) {
-    throw std::runtime_error{"Not implemented"};
+    std::string current;
+    std::unordered_set<std::string> words;
+    for(const char c: string) {
+        if (my_isalpha(c)) {
+            current.push_back(my_tolower(c));
+        }
+        else {
+            if (!current.empty()) {
+                words.insert(current);
+                current = "";
+            }
+        }
+    }
+    if (!current.empty()) {
+        words.insert(current);
+    }
+    return words.size();
 }
