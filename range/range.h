e9899b8b50ed443fe7f4e931928c0ce7b5ab8db2
andrey_voter <a.v0dyany@yandex.ru>
2023-10-01 12:09:11 +0300

    Add range task

--- range/range.h
+++ range/range.h
@@ -4,5 +4,19 @@
 #include <stdexcept>
 
 std::vector<int> Range(int from, int to, int step = 1) {
-    throw std::runtime_error{"Not implemented"};
+    if (step == 0 || (from == to) || ((from < to) && (step < 0)) || ((from > to) && (step > 0))) {
+        return std::vector<int>();
+    }
+    auto res = std::vector<int>();
+    if (from > to) {
+        for (int i = from; i > to; i += step) {
+            res.push_back(i);
+        }
+    }
+    else {
+        for (int i = from; i < to; i += step) {
+            res.push_back(i);
+        }
+    }
+    return res;
 }
