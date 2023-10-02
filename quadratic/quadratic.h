3e64285190fa3545d55d5865638c73e60a5ea677
andrey_voter <a.v0dyany@yandex.ru>
2023-10-03 00:09:42 +0300

    Add quadratic task

--- quadratic/quadratic.h
+++ quadratic/quadratic.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdexcept>
+#include <math.h>
 
 enum class RootCount { kZero, kOne, kTwo, kInf };
 
@@ -10,6 +11,43 @@ struct Roots {
     double second;
 };
 
+double GetDiscriminant(int a, int b, int c) {
+    return b * b - 4 * a * c;
+}
+
+std::pair<double, double> GetRoots(double discriminant, int a, int b) {
+    double x1 = (-b + sqrt(discriminant)) / (2 * a);
+    double x2 = (-b - sqrt(discriminant)) / (2 * a);
+    if (x1 > x2) {
+        std::swap(x1, x2);
+    }
+    return std::pair{x1, x2};
+}
+
 Roots SolveQuadratic(int a, int b, int c) {
-    throw std::runtime_error{"Not implemented"};
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                return Roots{RootCount::kInf, 0, 0};
+            } else {
+                return Roots{RootCount::kZero, 0, 0};
+            }
+        } else {
+            double root = -c / static_cast<double>(b);
+            return Roots{RootCount::kOne, root, 0};
+        }
+    } else {
+        double discriminant = GetDiscriminant(a, b, c);
+        if (discriminant < 0) {
+            return Roots{RootCount::kZero, 0, 0};
+        } else {
+            if (discriminant > 0) {
+                auto roots = GetRoots(discriminant, a, b);
+                return Roots{RootCount::kTwo, roots.first, roots.second};
+            } else {
+                double root = -b / static_cast<double>(2 * a);
+                return Roots{RootCount::kOne, root, 0};
+            }
+        }
+    }
 }
