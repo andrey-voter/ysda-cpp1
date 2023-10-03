94496dbb93727da75be871f0e3135da76233fba4
andrey_voter <a.v0dyany@yandex.ru>
2023-10-03 15:00:23 +0300

    Add pt-triangle task

--- pt-triangle/point_triangle.h
+++ pt-triangle/point_triangle.h
@@ -11,10 +11,10 @@ struct Triangle {
     Point a, b, c;
 };
 
-double TriangleArea(Point a, Point b, Point c) {
-    return 0.5 * std::abs(a.x * (static_cast<double>(b.y) - c.y) +
-                          b.x * (static_cast<double>(c.y) - a.y) +
-                          c.x * (static_cast<double>(a.y) - b.y));
+int64_t DoubleTriangleArea(Point a, Point b, Point c) {
+    return std::abs(a.x * (static_cast<int64_t>(b.y) - c.y) +
+                    b.x * (static_cast<int64_t>(c.y) - a.y) +
+                    c.x * (static_cast<int64_t>(a.y) - b.y));
 }
 
 bool IsPointInTriangle(const Triangle& t, const Point& pt) {
@@ -22,8 +22,8 @@ bool IsPointInTriangle(const Triangle& t, const Point& pt) {
         ((pt.x == t.c.x) && (pt.y == t.c.y))) {
         return true;
     }
-    double total_area = TriangleArea(t.a, t.b, t.c);
-    if (std::abs(total_area) < 1e-30) {
+    int64_t double_total_area = DoubleTriangleArea(t.a, t.b, t.c);
+    if (double_total_area == 0) {
         int min_x = std::min(std::min(t.a.x, t.b.x), t.c.x);
         int max_x = std::max(std::max(t.a.x, t.b.x), t.c.x);
 
@@ -57,20 +57,18 @@ bool IsPointInTriangle(const Triangle& t, const Point& pt) {
             if (max_x_y == min_x_y) {
                 return ((pt.y == max_x_y) && ((min_x < pt.x) && (pt.x < max_x)));
             } else {
-                double a =
-                    (static_cast<double>(max_x_y) - min_x_y) / (static_cast<double>(max_x) - min_x);
-                double b = static_cast<double>(min_x_y) - a * static_cast<double>(min_x);
-                if ((std::abs(a * static_cast<double>(pt.x) + b - static_cast<double>(pt.y)) <
-                     1e-30) &&
-                    ((min_x < pt.x) && (pt.x < max_x))) {
+                int64_t a = -(static_cast<int64_t>(max_x_y) - min_x_y);
+                int64_t b = static_cast<int64_t>(max_x) - min_x;
+                int64_t c = -(a * min_x + b * min_x_y);
+                if ((a * pt.x + b * pt.y + c == 0) && ((min_x < pt.x) && (pt.x < max_x))) {
                     return true;
                 }
             }
         }
         return false;
     }
-    double s1 = TriangleArea(t.a, t.b, pt);
-    double s2 = TriangleArea(t.a, t.c, pt);
-    double s3 = TriangleArea(t.c, t.b, pt);
-    return std::abs(total_area - s1 - s2 - s3) < 1e-30;
+    int64_t s1 = DoubleTriangleArea(t.a, t.b, pt);
+    int64_t s2 = DoubleTriangleArea(t.a, t.c, pt);
+    int64_t s3 = DoubleTriangleArea(t.c, t.b, pt);
+    return ((double_total_area - s1 - s2 - s3) == 0);
 }
