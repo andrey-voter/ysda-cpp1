696bc2933bae58fd169864dd03c6fd6cf82aaaf3
andrey_voter <a.v0dyany@yandex.ru>
2023-10-06 22:19:42 +0300

    Add stack task

--- stack/stack.h
+++ stack/stack.h
@@ -1,25 +1,37 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class Stack {
 public:
     void Push(int x) {
+        data_.push_back(x);
     }
 
     bool Pop() {
+        if (data_.empty()) {
+            return false;
+        }
+        data_.pop_back();
         return true;
     }
 
     int Top() const {
+        if (!data_.empty()) {
+            return data_[data_.size() - 1];
+        }
         return 0;
     }
 
     bool Empty() const {
-        return false;
+        return data_.empty();
     }
 
     size_t Size() const {
-        return 0;
+        return data_.size();
     }
+
+private:
+    std::vector<int> data_;
 };
