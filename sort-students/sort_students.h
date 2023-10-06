f701ae8f8a01e47be15fd6d6f7559207cdbb528e
andrey_voter <a.v0dyany@yandex.ru>
2023-10-06 17:25:34 +0300

    Add sort-students task

--- sort-students/sort_students.h
+++ sort-students/sort_students.h
@@ -11,6 +11,50 @@ struct Student {
 
 enum class SortType { kByName, kByDate };
 
-void SortStudents(std::vector<Student>* students, SortType sort_type) {
-    throw std::runtime_error{"Not implemented"};
+bool SortByName(const Student &student1, const Student &student2) {
+    if (student1.surname == student2.surname) {
+        if (student1.name == student2.name) {
+            if (student1.year == student2.year) {
+                if (student1.month == student2.month) {
+                    return student1.day < student2.day;
+                } else {
+                    return student1.month < student2.month;
+                }
+            } else {
+                return student1.year < student2.year;
+            }
+        } else {
+            return student1.name < student2.name;
+        }
+    } else {
+        return student1.surname < student2.surname;
+    }
+}
+
+bool SortByDate(const Student &student1, const Student &student2) {
+    if (student1.year == student2.year) {
+        if (student1.month == student2.month) {
+            if (student1.day == student2.day) {
+                if (student1.surname == student2.surname) {
+                    return student1.name < student2.name;
+                } else {
+                    return student1.surname < student2.surname;
+                }
+            } else {
+                return student1.day < student2.day;
+            }
+        } else {
+            return student1.month < student2.month;
+        }
+    } else {
+        return student1.year < student2.year;
+    }
+}
+
+void SortStudents(std::vector<Student> *students, SortType sort_type) {
+    if (sort_type == SortType::kByName) {
+        std::sort(students->begin(), students->end(), SortByName);
+    } else {
+        std::sort(students->begin(), students->end(), SortByDate);
+    }
 }
--- sort-students/test.cpp
+++ sort-students/test.cpp
@@ -1,6 +1,10 @@
 #include <sort_students.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
 
 #include <catch2/catch_test_macros.hpp>
+using Catch::Matchers::Equals;
 
-TEST_CASE("Your test") {
-}
+TEST_CASE("Simple") {
+//
+    }
