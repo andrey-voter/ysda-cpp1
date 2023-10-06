c5069830a2d4596f7167e88b61a2f7c0af348022
andrey_voter <a.v0dyany@yandex.ru>
2023-10-06 16:44:55 +0300

    Add rotation task

--- rotate/rotate.h
+++ rotate/rotate.h
@@ -4,6 +4,19 @@
 #include <vector>
 #include <cstddef>
 
+void Reverse(std::vector<int>& data, int start, int end) {
+    while (end > start) {
+        std::swap(data[start], data[end]);
+        end--;
+        start++;
+    }
+}
+
 void Rotate(std::vector<int>* data, size_t shift) {
-    throw std::runtime_error{"Not implemented"};
+    if (data->empty() or shift == 0) {
+        return;
+    }
+    Reverse(*data, 0, shift - 1);
+    Reverse(*data, shift, data->size() - 1);
+    Reverse(*data, 0, data->size() - 1);
 }
