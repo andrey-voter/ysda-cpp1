02bc194037471efba7503bf3685de9742d922ed3
andrey_voter <a.v0dyany@yandex.ru>
2023-10-08 19:37:53 +0300

    Add static-map task

--- static-map/static_map.h
+++ static-map/static_map.h
@@ -3,13 +3,26 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <algorithm>
 
 class StaticMap {
 public:
-    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items) {
+    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items)
+        : data_(items) {
+        std::sort(data_.begin(), data_.end(),
+                  [](const auto& a, const auto& b) { return a.first < b.first; });
     }
 
     bool Find(const std::string& key, std::string* value) const {
+        auto it = std::lower_bound(data_.begin(), data_.end(), key,
+                                   [](const auto& a, const auto& b) { return a.first < b; });
+        if (it != data_.end() and it->first == key) {
+            *value = it->second;
+            return true;
+        }
         return false;
     }
+
+private:
+    std::vector<std::pair<std::string, std::string>> data_;
 };
