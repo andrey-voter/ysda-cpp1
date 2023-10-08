5e330afea09c6c61df26a89502c6bd3430e3a802
andrey_voter <a.v0dyany@yandex.ru>
2023-10-08 20:03:33 +0300

    Add string view task

--- string-view/string_view.h
+++ string-view/string_view.h
@@ -1,3 +1,38 @@
 #pragma once
 
-class StringView {};
+#include <string>
+#include <cstring>
+
+class StringView {
+public:
+    StringView(const std::string& string, size_t pos = 0, size_t len = std::string::npos) {
+        data_ = string.c_str() + pos;
+        size_ = std::min(len, string.length() - pos);
+    }
+
+    StringView(const char* c_str) {
+        data_ = c_str;
+        size_ = strlen(c_str);
+    }
+
+    StringView(const char* c_str, size_t len) {
+        data_ = c_str;
+        size_ = len;
+    }
+
+    char operator[](int i) const {
+        return *(data_ + i * sizeof(char));
+    }
+
+    const char* Data() const {
+        return data_;
+    }
+
+    size_t Size() const {
+        return size_;
+    }
+
+private:
+    const char* data_;
+    size_t size_;
+};
