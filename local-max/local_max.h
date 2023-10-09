2ae4301b218773cfea09eba9c0c71c8809ab5215
andrey_voter <a.v0dyany@yandex.ru>
2023-10-09 23:38:07 +0300

    Add local-max task

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -4,5 +4,30 @@
 
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
-    throw std::runtime_error{"Not implemented"};
+    if (std::next(first) == last) {
+        return first;
+    }
+    if ((*(std::next(first))) < (*first)) {
+        return first;
+    }
+    Iterator prev = first;
+    first++;
+    Iterator ans = first;
+    while (first < last) {
+        Iterator next = std::next(first);
+        if (next != last) {
+            if ((*prev) < (*ans) and (*next) < (*ans)) {
+                return ans;
+            }
+        } else {
+            if ((*prev) < (*ans)) {
+                return ans;
+            } else {
+                return last;
+            }
+        }
+        ans++;
+        prev++;
+    }
+    return ans;
 }
