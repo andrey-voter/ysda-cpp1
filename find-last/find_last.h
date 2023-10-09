4193b8bebee65b38375b5d8087f75701e83c118c
andrey_voter <a.v0dyany@yandex.ru>
2023-10-09 22:02:13 +0300

    Add find_last task

--- find-last/find_last.h
+++ find-last/find_last.h
@@ -5,11 +5,12 @@
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& value) {
     Iterator ans = last;
-    while (first != last) {
-        if ((*first) == value) {
-            ans = first;
+    while (last != first) {
+        --last;
+        if ((*last) == value) {
+            ans = last;
+            break;
         }
-        first++;
     }
     return ans;
 }
