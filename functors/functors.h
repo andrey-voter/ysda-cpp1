92911e7321289587c1c9367b0735045b906bf5ef
andrey_voter <a.v0dyany@yandex.ru>
2023-10-10 22:11:19 +0300

    Add functors task

--- functors/functors.h
+++ functors/functors.h
@@ -1,12 +1,37 @@
 #pragma once
 
 #include <cstddef>
+#include <algorithm>
 
 template <class Functor>
-class ReverseUnaryFunctor {};
+class ReverseUnaryFunctor {
+public:
+    ReverseUnaryFunctor(Functor functor) : functor_(functor) {
+    }
+
+    template <class T>
+    bool operator()(const T& arg) const {
+        return !functor_(arg);
+    }
+
+private:
+    Functor functor_;
+};
 
 template <class Functor>
-class ReverseBinaryFunctor {};
+class ReverseBinaryFunctor {
+public:
+    ReverseBinaryFunctor(Functor functor) : functor_(functor) {
+    }
+
+    template <class T, class U>
+    bool operator()(const T& arg1, const U& arg2) const {
+        return functor_(arg2, arg1);
+    }
+
+private:
+    Functor functor_;
+};
 
 auto MakeReverseUnaryFunctor(auto functor) {
     return ReverseUnaryFunctor{functor};
@@ -17,4 +42,15 @@ auto MakeReverseBinaryFunctor(auto functor) {
 }
 
 template <class Iterator>
-size_t ComparisonsCount(Iterator first, Iterator last);
+size_t ComparisonsCount(Iterator first, Iterator last) {
+    size_t count = 0;
+
+    auto comparator = [&](const auto& arg1, const auto& arg2) {
+        ++count;
+        return arg1 < arg2;
+    };
+
+    std::sort(first, last, comparator);
+
+    return count;
+}
