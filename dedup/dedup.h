a1b61dd65c3a47cfcd5c98eb133912a089e61230
andrey_voter <a.v0dyany@yandex.ru>
2023-10-31 15:34:37 +0300

    Add dedup task

--- dedup/dedup.h
+++ dedup/dedup.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 template <class T>
 std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>& items) {
@@ -16,10 +17,15 @@ std::vector<std::unique_ptr<T>> Duplicate(const std::vector<std::shared_ptr<T>>&
 template <class T>
 std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items) {
     std::vector<std::shared_ptr<T>> res;
-    std::cout << items[0];
-//    for (size_t i = 0; i < items.size(); ++i) {
-//
-//        res.emplace_back(std::make_shared<T>(*items[i]).get());
-//    }
+    std::unordered_map<T, std::shared_ptr<T>> t_to_ptr;
+
+    for (size_t i = 0; i < items.size(); ++i) {
+        if (t_to_ptr.contains(*items[i])) {
+            res.push_back(t_to_ptr[*items[i]]);
+        } else {
+            t_to_ptr.insert({*items[i], std::make_shared<T>(*items[i])});
+            res.push_back(t_to_ptr[*items[i]]);
+        }
+    }
     return res;
 }
