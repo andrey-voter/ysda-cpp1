3e46bd9576a4edb545f9f25bd87327c311876822
andrey_voter <a.v0dyany@yandex.ru>
2023-11-11 20:48:35 +0300

    Add safe transform task

--- safe-transform/transform.h
+++ safe-transform/transform.h
@@ -6,15 +6,14 @@
 
 template <class It, class Pred, class Func>
 void TransformIf(It begin, It end, Pred p, Func f) {
-//    std::vector<typename std::iterator_traits<It>::value_type> log;
     std::vector<std::pair<It, typename std::iterator_traits<It>::value_type>> log;
     try {
         for (auto it = begin; it != end; ++it) {
             if (p(*it)) {
                 try {
                     log.emplace_back(it, *it);
+                } catch (...) {
                 }
-                catch (...) {}
                 f(*it);
             }
         }
