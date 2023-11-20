87f40dba1d2ea50e4227e228ac04d2099730cba6
andrey_voter <a.v0dyany@yandex.ru>
2023-11-20 14:27:01 +0300

    started pimpl

---
+++ pimpl/ugly.cpp
@@ -0,0 +1,11 @@
+#include "ugly.h"
+
+class Spline::SplineImpl {
+public:
+    // Реализация деталей класса SplineImpl
+};
+
+Spline::Spline(std::vector<double> x, std::vector<double> y, double a, double b) {
+    
+}
+
