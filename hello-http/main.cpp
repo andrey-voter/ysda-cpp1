8ce4148e3185221d60678a871ed62011d56be994
andrey_voter <a.v0dyany@yandex.ru>
2023-11-25 20:13:22 +0300

    main file for hello-http

--- hello-http/main.cpp
+++ hello-http/main.cpp
@@ -1,2 +1,10 @@
+#include "weather.h"
+#include <iostream>
+
 int main() {
+    auto a = CreateYandexForecaster("4dedd4ab-23f6-4217-8f58-d3ae32652510");
+    auto b = a->ForecastWeather();
+    std::cout << b.temp << b.feels_like << std::endl;
+
+    return 0;
 }
