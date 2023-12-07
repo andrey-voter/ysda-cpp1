ce61218491f1d0ce34e47a756fa8203d7c15f58d
andrey_voter <a.v0dyany@yandex.ru>
2023-12-07 16:35:16 +0300

    started bot

---
+++ bot/telegram/client.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class Controls {
+public:
+    void getMe();
+    void sendMessage();
+    void getUpdates();
+
+private:
+    std::string apy_key_ = "6410806161:AAEPAqtJ0CO4-f1Dzo-qK3OJmAuFIWjMFDU";
+};
--- bot/telegram/main.cpp
+++ bot/telegram/main.cpp
@@ -1,2 +1,5 @@
 int main(int argc, char* argv[]) {
+
+//    6410806161:AAEPAqtJ0CO4-f1Dzo-qK3OJmAuFIWjMFDU
+
 }
